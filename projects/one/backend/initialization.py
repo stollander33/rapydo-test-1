cd6e54bde47fd76c715f277148e5b876dc5bd9f6
Richardssen <richardssen33@gmail.com>
2022-12-14 13:48:27 +0100

    rapydo create

---
+++ projects/one/backend/initialization.py
@@ -0,0 +1,13 @@
+class Initializer:
+
+    """
+    This class is instantiated just after restapi init
+    Implement the constructor to add operations performed one-time at initialization
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    # This method is called after normal initialization if TESTING mode is enabled
+    def initialize_testing_environment(self) -> None:
+        pass
